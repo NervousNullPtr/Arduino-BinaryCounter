af8d6cf2a2f74006fd91a4cfbdd2946ba44807ca
NervousNullPtr <57870403+NervousNullPtr@users.noreply.github.com>
2021-12-23 22:12:20 +0100

    Added readme

---
+++ readme.md
@@ -0,0 +1,23 @@
+Binary Counting using Arduino and LEDs
+=====
+
+This repository is a quick example on how the binary system works. It shows numbers from 0-255 using 8 LEDs.
+
+Required Parts
+=====
+* **1** Arduino (With **8** outputs)
+* **1** Breadboard
+* **9** Wires
+* **8** Resistors (220 Ω)
+
+Structure
+=====
+![Oops, a picture should be here.](./Layout.png)
+
+Code
+=====
+When finished with the building, open the `src/main.c` file and simply copy 'n paste it into your Arduino IDE. Next, hit `Ctrl+Shift+N` in your Arduino IDE and copy the source code from `include/types.h` into that file, and save it as `types.h`. Now upload it to your Arduino and that's it - everything done!
+
+Running on the Arduino
+=====
+After you've uploaded your freshly compiled program, the 8 LEDs should light up sequentially (250ms delay) and stay on for half a second. Now the LEDs will be turned of and the program starts counting in `big endian` mode from 0 to 255 until you stop it(From right to left). Have fun!
--- src/main.c
+++ src/main.c
@@ -3,29 +3,34 @@
 
 #define LIGHTUP_DELAY 250
 
+u8 usedPins[7] = { 9, 8, 7, 6, 5, 4, 3, 2 };
+
 void setup() {
-	for (u8 i = 2; i < 10; i++) {
-		pinMode(i, OUTPUT);
+	for (u8 i = 0; i < 8; i++) {
+		pinMode(usedPins[i], OUTPUT); // Set all used pins to OUTPUT mode
 	}
-	for (u8 i = 2; i < 10; i++) {
-		digitalWrite(i, HIGH);
-		delay(LIGHTUP_DELAY);
+
+	for (u8 i = 0; i < 8; i++) {
+		digitalWrite(usedPins[i], HIGH); // Show that everything is running
+		delay(LIGHTUP_DELAY);  			 // Delay, so that it doesn't turn off instantly
 	}
-	for (u8 i = 2; i < 10; i++) {
-		digitalWrite(i, LOW);
+
+	for (u8 i = 0; i < 8; i++) {
+		digitalWrite(usedPins[i], LOW); // Turn of every LED
 	}
 }
 
-u8 usedPins[7] = { 9, 8, 7, 6, 5, 4, 3, 2 }; // Pins to be set low/high
-
 void loop() {
 	for (u8 num = 0; num < 256; num++) { // 8 LEDs => Counting up to 2^8
+
 		for (u8 j = 0; j < 8; j++) {
+
 			if ( (num & ( 0b00000001 << j ) ) >> j == 1)
 			{ digitalWrite(usedPins[j], HIGH); }
 			else
 			{ digitalWrite(usedPins[j], LOW); }
 		}
+		
 		delay(LIGHTUP_DELAY);
 	}
 }
