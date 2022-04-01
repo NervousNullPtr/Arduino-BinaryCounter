ad06b0c3eda8e97635d53ae05aee4d13d670ad31
Maximilian "NervousNullPtr" Schleicher <57870403+NervousNullPtr@users.noreply.github.com>
2022-04-01 13:51:00 +0200

    Formatting

--- src/main.c
+++ src/main.c
@@ -6,18 +6,16 @@
 u8 usedPins[8] = { 9, 8, 7, 6, 5, 4, 3, 2 };
 
 void setup() {
-	for (u8 i = 0; i < 8; i++) {
-		pinMode(usedPins[i], OUTPUT); // Set all used pins to OUTPUT mode
-	}
+	for (u8 i = 0; i < 8; i++)
+	{ pinMode(usedPins[i], OUTPUT); }  // Set all used pins to OUTPUT mode
 
 	for (u8 i = 0; i < 8; i++) {
 		digitalWrite(usedPins[i], HIGH); // Show that everything is running
 		delay(LIGHTUP_DELAY);  			 // Delay, so that it doesn't turn off instantly
 	}
 
-	for (u8 i = 0; i < 8; i++) {
-		digitalWrite(usedPins[i], LOW); // Turn of every LED
-	}
+	for (u8 i = 0; i < 8; i++)
+	{ digitalWrite(usedPins[i], LOW); } // Turn of every LED
 }
 
 void loop() {
